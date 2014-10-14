b9882ae21b20d633a06d6dd709acaf5e38cbdaba
cristopher2529 <cristopher2529@gmail.com>
2014-10-14 18:36:28 +0000

    Creacion de usuarios y Roles, Conexion con la base de dato

--- Json.php
+++ Json.php
@@ -1,8 +1,35 @@
 <?php
-   header("Content-Type: application/json");
+  // header("Content-Type: application/json");
+   
+   // echo  getenv("REMOTE_ADDR");
+   $ip =  $_SERVER['SERVER_ADDR'] . ":3306";
+   $port = "3306";
+   $user = "cristopher2529";
+   $db = "ESTUDIANTE";
+   
+   $conn = mysql_connect($ip, $user, '', $db)or die(mysql_error());
+   mysql_select_db($db,$conn)or die(mysql_error());
+   $result = mysql_query("select * from ROLES",$conn)or die(mysql_error());
+   
+   $json = [];
+   while ($fila = mysql_fetch_assoc($result)) {
+      echo $fila['NOMBRES'];
+      
 
-   echo json_encode(array(array( "Name" => "Alfreds Futterkiste", "City" => "Berlin", "Country" => "Germany" )
-   ,array( "Name" => "Berglunds snabbköp", "City" => "Luleå", "Country" => "Sweden")
-   ,array( "Name" => "dsaBerglunds snabbköp", "City" => "dsaLuleå", "Country" => "daSweden")));
+      $row = [];
+      foreach ($fila as $key => $valor) {
+          echo "$key: $valor  |  ";
+          $row[$key] =  $valor ;
+      }echo "<br>$";
+      var_dump($row);
+      // array_push($json, $row);
+      
+      
+   }echo "<br><br><br>";
+   var_dump($json);
+
+   // echo json_encode(array(array( "Name" => "Alfreds Futterkiste", "City" => "Berlin", "Country" => "Germany" )
+   // ,array( "Name" => "Berglunds snabbköp", "City" => "Luleå", "Country" => "Sweden")
+   // ,array( "Name" => "dsaBerglunds snabbköp", "City" => "dsaLuleå", "Country" => "daSweden")));
 
 ?>
