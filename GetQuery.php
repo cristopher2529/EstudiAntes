3f831c1c270abec3fcd0c42600aca1bf228044dd
cristopher2529 <cristopher2529@gmail.com>
2014-11-06 19:12:48 +0000

    Solo es necesario hacer el query y mandar los parametros

--- GetQuery.php
+++ GetQuery.php
@@ -1,90 +1,100 @@
 <?php
-// Notificar todos los errores de PHP
-// Lo mismo que error_reporting(E_ALL);
 
    //leo parametros
    $parameters = array();
    foreach ($_GET as $key=>$value) {
    //  echo "$key = " . urldecode($value) . "<br />\n";
-      array_push($parameters, urldecode($value));
-   }
-
-   //lEER QUERY
-   $query = file_get_contents("sql/getRoles.sql");
-   $resultSet = getResults($query, $parameters);
-
-
-   foreach($resultSet['Result'] as $key=>$value){
-      // echo $key.": ".$value."<br>";
-      // var_dump($value);
-      foreach($value as $key2=>$value2){
-         echo $key2.": ".$value2."<br>";
-      }echo "-------------------------------------------------------------------------<br>";
+      if($key!="query")//valido que el parametro no sea el nombre del query
+         array_push($parameters, urldecode($value));
    }
    
+   if(!isset($_GET["query"])){
+      echo "No especifico el query";
+   }else{
+      // echo $_GET["query"];
+
+      //lEER QUERY
+      $query = file_get_contents("sql/".$_GET["query"].".sql");
+      $resultSet = getResults($query, $parameters);
    
-   function getResults($query, $parameters){
-      // echo $query."<br>";
-      // foreach($parameters as $key){
-      //    echo $key;
-      // }
-      // echo "<br><br>";
+      // var_dump($parameters);
+      echo json_encode($resultSet);
       
+      //PARA VER LOS REGISTROS SEPARADOS POR LINEA
+      /* foreach($resultSet['Result'] as $key=>$value){
+      //    // echo $key.": ".$value."<br>";
+      //    // var_dump($value);
+      //    echo "<br><br>".json_encode($value)."<br><br>";
+      //    foreach($value as $key2=>$value2){
+      //       echo $key2.": ".$value2."<br>";
+      //    }echo "-------------------------------------------------------------------------<br>";
+      // }*/
       
-      $ip =  $_SERVER['SERVER_ADDR'] . "";
-      $port = "3306";
-      $user = "cristopher2529";
-      $db = "ESTUDIANTE";
-      
-      $mysqli = new mysqli($ip, $user, '', $db);
-      /* verificar conexión */
-      if (mysqli_connect_errno()) {
-          printf("Connect failed: %s\n", mysqli_connect_error());
-          exit();
-      }
-      
-        /* crear una sentencia preparada */
-      if ($stmt = $mysqli->prepare($query)){
+   }
    
-   
-         foreach($parameters as $key){
-            $stmt->bind_param('s', $key);
+      //esta funcion hace la conexion, la consulta y le pasa los parametros
+      function getResults($query, $parameters){
+         // echo $query."<br>";
+         // foreach($parameters as $key){
+         //    echo $key;
+         // }
+         // echo "<br><br>";
+         
+         
+         $ip =  $_SERVER['SERVER_ADDR'] . "";
+         $port = "3306";
+         $user = "cristopher2529";
+         $db = "ESTUDIANTE";
+         
+         $mysqli = new mysqli($ip, $user, '', $db);
+         /* verificar conexión */
+         if (mysqli_connect_errno()) {
+             printf("Connect failed: %s\n", mysqli_connect_error());
+             exit();
          }
          
-          /* ligar parámetros para marcadores */
-         //  $stmt->bind_param("s", $city);
+           /* crear una sentencia preparada */
+         if ($stmt = $mysqli->prepare($query)){
       
-          /* ejecutar la consulta */
-          $stmt->execute();
-   
-          /* ligar variables de resultado */
-         //  $stmt->bind_result($rol,$DESCRIPCION);
-         //  while ($stmt->fetch()) {
-         //     echo $rol . " - ";
-         //     echo $DESCRIPCION ."<br/>";
-         //  }
+      
+            foreach($parameters as $key){
+               $stmt->bind_param('s', $key);
+            }
+            
+             /* ligar parámetros para marcadores */
+            //  $stmt->bind_param("s", $city);
          
-         $row = bind_result_array($stmt);
-         if(!$stmt->error)
-          {
-             while($stmt->fetch()){
-                foreach($row as $key=>$value){
-                  $results['Result'][] = getCopy($row);
-                   break;
+             /* ejecutar la consulta */
+             $stmt->execute();
+      
+             /* ligar variables de resultado */
+            //  $stmt->bind_result($rol,$DESCRIPCION);
+            //  while ($stmt->fetch()) {
+            //     echo $rol . " - ";
+            //     echo $DESCRIPCION ."<br/>";
+            //  }
+            
+            $row = bind_result_array($stmt);
+            if(!$stmt->error)
+             {
+                while($stmt->fetch()){
+                   foreach($row as $key=>$value){
+                     $results['Result'][] = getCopy($row);
+                      break;
+                   }
                 }
              }
-          }
-         // var_dump($results);
-   
-          /* cerrar sentencia */
-           $stmt->close();
-          return $results;
+            // var_dump($results);
+      
+             /* cerrar sentencia */
+              $stmt->close();
+             return $results;
+         }
+         
+         $mysqli->close();
+         return null;
+         
       }
-      
-      $mysqli->close();
-      return null;
-      
-   }
       
       //esta funcion me devuelve un array con todas las columans que retorna el query
       function bind_result_array($stmt){
